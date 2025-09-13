dd297429f5c87f30a0dcd0b818f2951049f7ac1f
Reza Hosseini <moha.rezahosseini@gmail.com>
2025-09-13 07:32:46 +0200

    add new jinja loop functionality in template

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,14 @@ def index():
     user = {
         "username": "Reza"
     }
-    return render_template('index.html', user=user)
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "Beautiful day in Portland!"
+        },
+        {
+            "author": {"username": "Susan"},
+            "body": "The Avengers movie was so cool!"
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
