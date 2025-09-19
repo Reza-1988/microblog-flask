1837666e0b2ac756c42c3fd09664e04317d6281a
Reza Hosseini <moha.rezahosseini@gmail.com>
2025-09-19 17:15:20 +0200

    Add migration repository for tracking database schema
    Update .gitignore and remove ignored files from tracking

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_sqlalchemy import Migrate
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
