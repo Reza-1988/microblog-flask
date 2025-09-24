e564a2da908ef05e01d15c95f50dd9e4b366c56f
Reza Hosseini <moha.rezahosseini@gmail.com>
2025-09-24 18:09:58 +0200

    implement Logout route and make Logout link in navigation bar

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, flash, url_for
 from app import app, db
 from app.forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 import sqlalchemy as sa
 from app.models import User
 
@@ -54,3 +54,9 @@ def login():
 
     # GET request (or failed POST): render the template with the form (and any flashed errors).
     return render_template('login.html',title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
