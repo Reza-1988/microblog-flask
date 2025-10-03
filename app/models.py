bd18df4ebfe78d86796d62205b4c284f3e423d56
Reza Hosseini <moha.rezahosseini@gmail.com>
2025-10-03 12:55:06 +0200

    add new fields in user model and user profile template: last_seen, about_me

--- app/models.py
+++ app/models.py
@@ -13,9 +13,14 @@ class User(UserMixin, db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    about_me:so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    last_seen:so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda:datetime.now(timezone.utc))
 
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
     # so.WriteOnlyMapped, defines posts as a collection type with Post objects inside.
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
