2bb68fadc742b3053c8b6c3bd405af27056e6c60
Reza Hosseini <moha.rezahosseini@gmail.com>
2025-10-14 19:01:53 +0200

    add association table for follower and followed user

--- app/models.py
+++ app/models.py
@@ -1,29 +1,45 @@
 from datetime import datetime, timezone
-from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 from typing import Optional
+
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
 from flask_login import UserMixin
-from app import login
-from hashlib import md5
+from sqlalchemy import PrimaryKeyConstraint
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from app import db, login
+
+
+followers = sa.Table(
+    'followers',
+    db.metadata,
+    sa.Column('follower_id', sa.Integer, sa.ForeignKey('user.id'),
+              primary_key=True),
+    sa.Column('followed_id', sa.Integer, sa.ForeignKey('user.id'),
+              primary_key=True)
+    )
+
+
+
+
+
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-    about_me:so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-    last_seen:so.Mapped[Optional[datetime]] = so.mapped_column(
-        default=lambda:datetime.now(timezone.utc))
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
 
     # so.WriteOnlyMapped, defines posts as a collection type with Post objects inside.
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
-
-
+    posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return "<User {}>".format(self.username)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -32,11 +48,11 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def avatar(self, size):
-        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
         return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
 
 
-@ login.user_loader
+@login.user_loader
 def load_user(id):
     # Flask-Login calls this function automatically
     # It receives the user ID stored in the session
@@ -48,7 +64,7 @@ class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
     timestamp: so.Mapped[datetime] = so.mapped_column(
-        index=True, default=lambda:datetime.now(timezone.utc)
+        index=True, default=lambda: datetime.now(timezone.utc)
     )
     # NOTE:
     # We use `lambda: datetime.now(timezone.utc)` instead of `datetime.now(timezone.utc)`.
@@ -58,7 +74,8 @@ class Post(db.Model):
     #   is created, so each record gets the *current* timestamp at insertion.
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id))
 
-    author: so.Mapped[User] = so.relationship(back_populates='posts')
+    author: so.Mapped[User] = so.relationship(back_populates="posts")
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return "<Post {}>".format(self.body)
+
